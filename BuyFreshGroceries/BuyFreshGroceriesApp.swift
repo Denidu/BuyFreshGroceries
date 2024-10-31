0778072bc58b836030d371af0df2648cc9c56a21
Denidu Gamage <denidugamage21@gmail.com>
2024-10-31 08:35:28 +0530

    Initial Commit

---
+++ BuyFreshGroceries/BuyFreshGroceriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuyFreshGroceriesApp.swift
+//  BuyFreshGroceries
+//
+//  Created by Denidu Gamage on 2024-10-31.
+//
+
+import SwiftUI
+
+@main
+struct BuyFreshGroceriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
