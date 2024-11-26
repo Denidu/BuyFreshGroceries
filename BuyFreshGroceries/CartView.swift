e302886ad602ca9ccfc6ffa5ad4e724a3a27a2e4
Denidu Gamage <denidugamage21@gmail.com>
2024-11-26 11:44:27 +0530

    Initial Commit

---
+++ BuyFreshGroceries/CartView.swift
@@ -0,0 +1,18 @@
+//
+//  CartView.swift
+//  BuyFreshGroceries
+//
+//  Created by Denidu Gamage on 2024-10-31.
+//
+
+import SwiftUI
+
+struct CartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CartView()
+}
---
+++ BuyFreshGroceries/ShopModel/ShopItem.swift
@@ -0,0 +1,8 @@
+//
+//  ShopItem.swift
+//  BuyFreshGroceries
+//
+//  Created by Denidu Gamage on 2024-10-31.
+//
+
+import Foundation
